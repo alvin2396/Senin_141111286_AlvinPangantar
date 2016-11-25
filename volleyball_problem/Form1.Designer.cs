87fd59c4aaa34dfb3f36e9a72c111ff25641cc56
alvin2396 <alvin2396@yahoo.com>
2016-11-25 22:41:19 +0700

    tugas volleyball problem

---
+++ volleyball_problem/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace volleyball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.BtnHitung = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score 1";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(83, 39);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(133, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Score 2";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(87, 93);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(128, 20);
+            this.txt2.TabIndex = 2;
+            // 
+            // BtnHitung
+            // 
+            this.BtnHitung.Location = new System.Drawing.Point(88, 132);
+            this.BtnHitung.Name = "BtnHitung";
+            this.BtnHitung.Size = new System.Drawing.Size(127, 21);
+            this.BtnHitung.TabIndex = 3;
+            this.BtnHitung.Text = "Hitung";
+            this.BtnHitung.UseVisualStyleBackColor = true;
+            this.BtnHitung.Click += new System.EventHandler(this.BtnHitung_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Hasil";
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Location = new System.Drawing.Point(88, 182);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.ReadOnly = true;
+            this.txtHasil.Size = new System.Drawing.Size(127, 20);
+            this.txtHasil.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 261);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.BtnHitung);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button BtnHitung;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHasil;
+    }
+}
+
---
+++ volleyball_problem/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int pascal(int a, int b, int mod)
+        {
+            //Membuat Segitiga Pascal
+            int[] segitiga = new int[b];
+
+            //Nilai Awal Segitiga pascal = 1
+            for (int i = 0; i < b; i++)
+            {
+                segitiga[i] = 1;
+                //   MessageBox.Show(string.Join(",",segitiga));
+            }
+
+            //Pembuatan Segitiga sebanyak inputan
+            for (int i = b - 2; i > 0; i--)
+            {
+                for (int j = i; j < b - 1; j++)
+                {
+                    segitiga[j] += segitiga[j + 1];
+                    //MessageBox.Show(string.Join(",", segitiga));
+                    segitiga[j] %= mod;
+                    //MessageBox.Show(string.Join(",", segitiga));
+                }
+            }
+
+            //Perhitungan kemungkinan main
+            int kemungkinan = 1; //jika ada permainan maka minimal permainan = 1
+            for (int i = 0; i < a; i++) //a = 25
+            {
+                for (int j = 0; j < b - 1; j++) // b == 9
+                {
+                    segitiga[j] += segitiga[j + 1]; //segitiga[0] += segitiga[1];
+                    //MessageBox.Show(string.Join(",", segitiga));
+                    segitiga[j] %= mod;
+                    // MessageBox.Show(string.Join(",", segitiga));
+                }
+
+                kemungkinan += segitiga[0];
+                //MessageBox.Show(game.ToString());
+                kemungkinan %= mod;
+            }
+            return kemungkinan;
+        }
+
+        public long ex(long x, long exp_a, int mod)
+        {
+            long result = 1;
+            while (exp_a > 0)
+            {
+                if (exp_a % 2 == 1)
+                {
+                    result *= x;
+                    result %= mod;
+                }
+                exp_a /= 2;
+                x *= x;
+                x %= mod;
+            }
+            //MessageBox.Show(result.ToString());
+            return result % mod;
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnHitung_Click(object sender, EventArgs e)
+        {
+            int mod = 1000000007;
+            int a = Convert.ToInt32(txt1.Text);
+            int b = Convert.ToInt32(txt2.Text);
+            int temp;
+            long hasil;
+            // membuat nilai a > b
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+
+            if (a < 25)
+            {
+                txtHasil.Text = "0";
+            }
+            else if (a > 25 && a - b != 2)
+            {
+                txtHasil.Text = "0";
+            }
+            else if (a == 25 && a - b != 2)
+            {
+                hasil = (long)pascal(a - 1, b, mod);
+                txtHasil.Text = hasil.ToString();
+            }
+            else if (a > 25 && b == a - 2)
+            {
+                hasil = (long)pascal(24, 24, mod);
+                //MessageBox.Show(hasil.ToString());
+                int exp_a = b - 24;
+                //MessageBox.Show("hasil : ");
+                //MessageBox.Show(exp_a.ToString());
+                hasil *= ex(2, exp_a, mod);
+                hasil %= mod;
+                txtHasil.Text = hasil.ToString();
+            }
+        }
+    }
+}
